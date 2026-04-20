94693474d0133a02fae3ca870cc1bc7d2f4b7e3b
Camila <yagimaj@gmail.com>
2026-04-20 17:07:10 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package dobackofront;
+import java.util.Random;
+public class Main {
+    public   static void main(String [] args) {
+        Random random = new Random();
+
+        int numero = random.nextInt(100) + 1;
+
+        System.out.println("Número gerado:" + numero);
+
+        }
+    }
+
